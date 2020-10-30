396d806acedca41725927fa731980ff0ab0fa567
Nihaal <TigerThePredator>
2020-10-29 20:48:43 -0400

    Added queue.

--- twitter_component.py
+++ twitter_component.py
@@ -9,13 +9,15 @@ import queue
 class TwitterComponent:
     api = None # Used to access the Twitter API
     api_lock = None # Lock that must be acquired before using the api
+    twitter_queue = None # Queue for Twitter Statuses
     TIMEOUT = 20*60 # Max timeout in seconds (equal to twenty minutes)
 
     def __init__(self):
+        self.twitter_queue = queue.Queue()
         self.api_lock = threading.Lock()
+        auth = tweepy.OAuthHandler("OoI0dqhS0j0JzHbv14fqvaSna", "T9h8zp0uEWElLffNjk4vkaDHUjkUTJjPY6CpnZFEm2OfPGUXV7")
+        auth.set_access_token("1304199818517123072-P6Pj4lxNXNowOG0NFXPnlEnujlbPBx", "L7oxgElytNmIkXwcfw5Vkoy7ROrvERMAFpGqiyi6e1eI0")
         self.api_lock.acquire(blocking=True)
-        auth = tweepy.OAuthHandler("Xuu8AlgNIdcnD3XeWUc325mId", "zlzjJp4B783U1KzYaAJBWykYfk6haihCr1i8czNTAdEKlgy42W")
-        auth.set_access_token("1304199818517123072-tCRZLdNkX7GFtafjyUdZJHSr47t6uT", "YFbTORPAaw3XTy2L9BYIJpaKa7xwhXm53s7kmT0mtYrlL")
         self.api = tweepy.API(auth)
         self.api_lock.release()
 
@@ -40,47 +42,26 @@ class TwitterComponent:
                     not_deleted_mentions.append(mention)
             mentions = not_deleted_mentions
 
+            # TODO: Do input validation for mentions.
+            # Delete mentions that do not have a valid image or do not have a name of a style
+
             if(len(mentions) > 0):
                 # Set last_time equal to the created_at value for the latest mention
                 last_time = mentions[0].created_at
 
                 print("Enqueue Thread: Adding mentions to the queue.")
-                """
+                for mention in mentions:
+                    user_to_reply_to = mention.user.screen_name
+                    if(self.twitter_queue.qsize() > 20):
+                        # Send a Tweet to the user that they weren't added to the queue
+                        print("Enqueue Thread: Rejected " + user_to_reply_to + "\'s request.")
+                        self.api.update.status("@" + user_to_reply_to + " Sorry, I currently have too many requests in my queue. Your request cannot be completed.")
+                    else:
+                        print("Enqueue Thread: Accepted " + user_to_reply_to + "\'s request.")
+                        self.api.update_status("@" + user_to_reply_to + " you have been added to the queue.", in_reply_to_status_id=mention.id)
+                        self.twitter_queue.put(mention)
 
-                TODO: Add all of those mentions to the queue here.
-
-                The "mentions" list contains a list of Status objects that
-                need to be added to the queue. Keep in mind that if there
-                are already objects in the queue, then we need to:
-
-                1.) Make sure that the queue length does not exceed 20
-                    Status objects.
-
-                2.) Make sure that we keep track of which Status objects
-                    are not being added onto the queue. We will need to
-                    send a reply to those people stating that they could
-                    not be added onto the queue.
-
-                """
-
-
-                #loops through the mentions list and add them to the queue
-                #creates a list of items not added to the queue
-                q = queue.Queue()
-                for i in range(len(mentions)):
-                    if(q.qsize > 20){
-                        print("Sorry your request cannot be completed at this time")
-                        not_added = mentions[i:len(mentions)]
-                        break
-                    }
-                    else{
-                        q.put(mentions[i])
-                    }
-
-
-
-
-            # Wait for one minute
+            # Wait for 20 seconds
             print("Enqueue Thread: Waiting.")
             time.sleep(20)
 
@@ -88,6 +69,9 @@ class TwitterComponent:
     # If there are no mentions, then this will wait until there is a mention
     def __prune_queue(self, none):
         assert threading.current_thread().name == "dequeue_thread"
+        time.sleep(3)
+        for mention in iter(self.twitter_queue.get, None):
+            print(mention.text)
 
         """
 
